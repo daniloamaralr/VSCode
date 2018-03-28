dd549a50cb9b7f217a926853ee464c65cbc65547
daniloamaralr <daniloamaralr@gmail.com>
2018-03-27 21:33:17 -0300

    Initial Commit

---
+++ programa.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hoje é dia " . date('d/m/Y');
+echo " agora são " . date('H:i:s');
+?>
+
+<h1><?php echo "Título dentro do H1"; ?> </h1>
+
+<html>
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sáb</th>
+        </tr>
+    </table>
+</html>
